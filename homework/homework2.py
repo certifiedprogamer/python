3f6d91d1d30ede52b24ed53ca5c43b919a680cd2
certifiedprogamer <sowerskerry2@gmail.com>
2025-03-04 09:12:51 -0500

    unittest

---
+++ homework/homework2.py
@@ -0,0 +1,39 @@
+rain = input("Is it raining? (y/n)")
+homework = input("Do you have homework? (y/n)")
+if rain == "y" and homework == "y":
+    print("stay home and do homework")
+else:
+    print("Go out and watch a movie.")
+
+# savings = list()
+# for i in range(7):
+#     save = int(input("How much did you save today?: "))
+#     savings.append(save)
+# total = 0
+# for i in savings:
+#     total += i
+# print(f"Total money saved: ${total}")
+
+
+# assign 3
+# age = int(input("Enter your age: "))
+# student = input("Are you a student? (y/n): ")
+# if age < 25 and student == "y" or student == "yes":
+#     price = 32
+# else:
+#     price = 40
+# print(f"Membership price: ${price}")
+
+# assign 2
+# price = int(input("Enter the price of your item:"))
+# if price > 50:
+#    price -= 10
+# print(f"Final price: ${price}")
+
+
+# assign 1
+# age = int(input("Enter your age:"))
+# if age >= 16:
+#     print("You are eligible to get a driver's license.")
+# else:
+#     print("You are not old enough to drive.")
